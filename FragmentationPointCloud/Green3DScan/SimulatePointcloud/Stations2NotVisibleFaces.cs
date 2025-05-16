3ecf618402960c80d68218620f27120286effb3f
mezumroka <mezumroka@users.noreply.github.com>
2025-05-16 16:15:27 +0200

    Rest

--- FragmentationPointCloud/Green3DScan/SimulatePointcloud/Stations2NotVisibleFaces.cs
+++ FragmentationPointCloud/Green3DScan/SimulatePointcloud/Stations2NotVisibleFaces.cs
@@ -8,7 +8,7 @@ using Autodesk.Revit.UI;
 using JetBrains.Annotations;
 using Serilog;
 using CoordinateSystem = GeometryLib.D3.CoordinateSystem;
-using S = ScantraIO.Data;
+using S = Revit.Data;
 using Sys = System.Globalization.CultureInfo;
 using Path = System.IO.Path;
 using Vector = GeometryLib.D3.Vector;
