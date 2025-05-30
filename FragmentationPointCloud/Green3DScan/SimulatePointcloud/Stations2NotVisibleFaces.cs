31280ae3d2a47d89e11d701267f18166e109d334
mezumroka <mezumroka@users.noreply.github.com>
2025-05-30 18:36:31 +0200

    *

--- FragmentationPointCloud/Green3DScan/SimulatePointcloud/Stations2NotVisibleFaces.cs
+++ FragmentationPointCloud/Green3DScan/SimulatePointcloud/Stations2NotVisibleFaces.cs
@@ -1,204 +1,180 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using JetBrains.Annotations;
 
-using Revit.Green3DScan.SimulatePointCloud;
-
 using Serilog;
-//using CoordinateSystem = GeometryLib.D3.CoordinateSystem;
-using S = Revit.Data;
-//using Sys = System.Globalization.CultureInfo;
-//using Path = System.IO.Path;
-//using Vector = GeometryLib.D3.Vector;
+using RD = Revit.Data;
 
-namespace Revit.Green3DScan.SimulatePointCloud
+namespace Revit.Green3DScan.SimulatePointCloud;
+
+// TODO: Combine code with Bim2Stations.cs to avoid duplication of logic
+[Transaction(TransactionMode.Manual)]
+[UsedImplicitly]
+public class Stations2NotVisibleFaces : IExternalCommand
 {
-    [Transaction(TransactionMode.Manual)]
-    [UsedImplicitly]
-    public class Stations2NotVisibleFaces : IExternalCommand
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        public const string CsvHeader = "ObjectGuid;ElementId;East;North;Elevation";
-        private string path;
-
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        // Initialization
+        if (!ExternalCommandHelper.GetProjectPath(commandData, out string projectPath, out var document, out var uiDocument))
         {
-            #region setup
-
-            // settings json
-            var set = SettingsJson.ReadSettingsJson(Constants.pathSettings);
-
-            UIDocument uidoc = commandData.Application.ActiveUIDocument;
-            Document doc = uidoc.Document;
-            try
-            {
-                path = Path.GetDirectoryName(doc.PathName);
-                var fileInfo = new FileInfo(path);
-                DateTime date = fileInfo.LastWriteTime;
-            }
-            catch (Exception)
-            {
-                TaskDialog.Show("Message", "The file has not been saved yet.");
-                return Result.Failed;
-            }
-
-            // logger
-            string logsPath = Path.Combine(path, "00_Logs/");
-            if (!Directory.Exists(logsPath)) Directory.CreateDirectory(logsPath);
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.File(Path.Combine(logsPath, "LogFile_"), rollingInterval: RollingInterval.Minute)
-                .CreateLogger();
-            Log.Information("start Stations2NotVisibleFaces");
-            Log.Information(set.BBox_Buffer.ToString());
-
-            Transform trans = Helper.GetTransformation(doc, set, out CoordinateSystem crs);
-
-            #endregion setup
-
-            Log.Information("setup");
-
-            #region select files
-
-            // Faces
-            var fodPfRevit = new FileOpenDialog("CSV file (*.csv)|*.csv");
-            fodPfRevit.Title = "Select CSV file with BimFaces from Revit!";
-            if (fodPfRevit.Show() == ItemSelectionDialogResult.Canceled) return Result.Cancelled;
-            string csvPathPfRevit = ModelPathUtils.ConvertModelPathToUserVisiblePath(fodPfRevit.GetSelectedModelPath());
-
-            var fodRpRevit = new FileOpenDialog("CSV file (*.csv)|*.csv");
-            fodRpRevit.Title = "Select CSV file with BimFacesPlanes fromRevit!";
-            if (fodRpRevit.Show() == ItemSelectionDialogResult.Canceled) return Result.Cancelled;
-            string csvPathRpRevit = ModelPathUtils.ConvertModelPathToUserVisiblePath(fodRpRevit.GetSelectedModelPath());
-
-            #endregion select files
-
-            Log.Information("select files");
-
-            #region read files
-
-            var facesRevit = S.PlanarFace.ReadCsv(csvPathPfRevit, out string[] lineErrors1, out string error1);
-            var facesMap = new Dictionary<S.Id, S.PlanarFace>();
-            foreach (S.PlanarFace pf in facesRevit) facesMap[pf.Id] = pf;
-
-            var referencePlanesRevit =
-                S.ReferencePlane.ReadCsv(csvPathRpRevit, out string[] lineErrors2, out string error2);
-
-            var stations = Helper.CollectFamilyInstances(doc, trans, "ScanStation");
-
-            #endregion read files
-
-            Log.Information("read files");
-
-            #region write stations to csv
-
-            string csvPath = Path.Combine(path, "07_Stations/");
-
-            if (!Directory.Exists(csvPath)) Directory.CreateDirectory(csvPath);
-
-            using StreamWriter csv = File.CreateText(Path.Combine(csvPath, "Stations.csv"));
-            csv.WriteLine(CsvHeader);
-
-            foreach (XYZ item in stations)
-            {
-                csv.WriteLine(item.X.ToString(Sys.InvariantCulture) + ";" + item.Y.ToString(Sys.InvariantCulture) +
-                              ";" + item.Z.ToString(Sys.InvariantCulture));
-            }
-
-            csv.Close();
-
-            var listVector = new List<Vector>();
-            foreach (XYZ item in stations) listVector.Add(new Vector(item.X, item.Y, item.Z));
-            Log.Information(stations.Count + " stations");
-
-            #endregion read files
-
-            Log.Information("write stations to csv");
-
-            #region visible and not visible faces
-
-            var visibleFacesId = RayCasting.VisibleFaces(facesRevit, referencePlanesRevit, listVector, set,
-                out var pointClouds, out var test, out var hashPMin);
-
-            //Test 
-            var y = 0;
-            foreach (var item in test)
-                //Log.Information(item.Key.ToString());
-                //Log.Information(item.Value.ToString());
-                y += item.Value;
-            Log.Information(y.ToString());
-
-            var visibleFaceId = new HashSet<S.Id>();
-            var visibleFaces = new HashSet<S.PlanarFace>();
-            var visibleRefPlanes = new HashSet<S.ReferencePlane>();
-            var pFMap = new Dictionary<S.Id, S.PlanarFace>();
-            foreach (S.PlanarFace pf in facesRevit) pFMap[pf.Id] = pf;
-            for (var i = 0; i < stations.Count; i++)
-            {
-                foreach (S.Id id in visibleFacesId[i])
-                {
-                    visibleFaces.Add(pFMap[id]);
-                    visibleRefPlanes.Add(referencePlanesRevit[pFMap[id].ReferencePlaneId]);
-                    visibleFaceId.Add(id);
-                }
-            }
-
-            // visible faces
-            S.PlanarFace.WriteCsv(Path.Combine(path, "VisibleFaces.csv"), visibleFaces);
-            S.ReferencePlane.WriteCsv(Path.Combine(path, "VisibleRefPlanes.csv"), visibleRefPlanes);
-
-            var notVisibleFacesId = new List<S.Id>();
-            var notVisibleFaces = new List<S.PlanarFace>();
-            var notvisibleRefPlanes = new HashSet<S.ReferencePlane>();
-
-            var facesIdList = new List<S.Id>();
-            foreach (var item in facesMap) facesIdList.Add(item.Key);
-
-            // not visible faces
-            foreach (S.Id item in facesIdList)
-            {
-                if (!visibleFaceId.Contains(item))
-                {
-                    notVisibleFacesId.Add(item);
-                    notvisibleRefPlanes.Add(referencePlanesRevit[facesMap[item].ReferencePlaneId]);
-                    notVisibleFaces.Add(facesMap[item]);
-                }
-            }
-
-            S.PlanarFace.WriteCsv(Path.Combine(path, "NotVisibleFaces.csv"), notVisibleFaces);
-            S.ReferencePlane.WriteCsv(Path.Combine(path, "NotVisibleRefPlanes.csv"), notvisibleRefPlanes);
-
-            #endregion visible and not visible faces
-
-            Log.Information("visible and not visible faces");
-
-            #region color not visible faces
-
-            ElementId[] matId = default;
-
-            // add materials and save the ElementIds in a DataStorage
-            try
-            {
-                matId = Helper.AddMaterials(doc);
-            }
-            catch (Exception)
-            {
-                matId = Helper.ReadMaterialsDS(doc);
-            }
-
-            //Helper.Paint.ColourFace(doc, notVisibleFacesId, matId[0]);
-            Helper.Paint.ColourFace(doc, hashPMin.ToList(), matId[5]);
-
-            #endregion color not visible faces
-
-            Log.Information("color not visible faces");
-            TaskDialog.Show("Message", "finish");
-            Log.Information("end Stations2NotVisibleFaces");
-            return Result.Succeeded;
+            TaskDialog.Show("Message", "The project file has not been saved yet.");
+            return Result.Failed;
         }
+        ExternalCommandHelper.InitLogger(projectPath);
+        var settings = SettingsJson.ReadSettingsJson(Constants.pathSettings);
+
+        Log.Information("start Stations2NotVisibleFaces");
+        Log.Information(settings.BBox_Buffer.ToString());
+
+        // Get transformation
+        var transform = Helper.GetTransformation(document, settings);
+ 
+        Log.Information("setup");
+
+        #region select files
+
+        // Faces
+        var fodPfRevit = new FileOpenDialog(Stations.CsvFilter)
+        {
+            Title = "Select CSV file with BimFaces from Revit!"
+        };
+        if (fodPfRevit.Show() == ItemSelectionDialogResult.Canceled) return Result.Cancelled;
+        string csvPathPfRevit = ModelPathUtils.ConvertModelPathToUserVisiblePath(fodPfRevit.GetSelectedModelPath());
+
+        var fodRpRevit = new FileOpenDialog(Stations.CsvFilter)
+        {
+            Title = "Select CSV file with BimFacesPlanes fromRevit!"
+        };
+        if (fodRpRevit.Show() == ItemSelectionDialogResult.Canceled) return Result.Cancelled;
+        string csvPathRpRevit = ModelPathUtils.ConvertModelPathToUserVisiblePath(fodRpRevit.GetSelectedModelPath());
+
+        #endregion select files
+
+        Log.Information("select files");
+
+        #region read files
+
+        var facesRevit = RD.PlanarFace.ReadCsv(csvPathPfRevit, out _, out _);
+        var referencePlanesRevit = RD.ReferencePlane.ReadCsv(csvPathRpRevit, out _, out _);
+
+        var stations = Stations.CollectFromFamilyInstances(document, transform);
+
+        #endregion read files
+
+        Log.Information("read files");
+
+        #region write stations to csv
+
+        if(!Stations.WriteCsv(projectPath, stations)) 
+        {
+            Log.Error("Error writing stations to CSV.");
+            return Result.Failed;
+        }
+
+        Log.Information(stations.Count + " stations");
+
+        #endregion read files
+
+        Log.Information("write stations to csv");
+
+        #region visible and not visible faces
+
+        var visibleFacesId = RayCasting.VisibleFaces(facesRevit, referencePlanesRevit, stations, settings, out var test, out _);
+
+        //Test 
+        int y = 0;
+        foreach (var item in test)
+            //Log.Information(item.Key.ToString());
+            //Log.Information(item.Value.ToString());
+            y += item.Value;
+        Log.Information(y.ToString());
+
+        var visibleFaceId = new HashSet<RD.Id>();
+        var visibleFaces = new HashSet<RD.PlanarFace>();
+        var visibleRefPlanes = new HashSet<RD.ReferencePlane>();
+
+        for (int i = 0; i < stations.Count; i++)
+        {
+            foreach (var id in visibleFacesId[i])
+            {
+                visibleFaces.Add(facesRevit[id]);
+                visibleRefPlanes.Add(referencePlanesRevit[facesRevit[id].ReferencePlaneId]);
+                visibleFaceId.Add(id);
+            }
+        }
+
+        // visible faces
+        RD.PlanarFace.WriteCsv(Path.Combine(projectPath, Stations.VisibleFacesFileName), visibleFaces);
+        RD.ReferencePlane.WriteCsv(Path.Combine(projectPath, Stations.VisibleRefPlanesFileName), visibleRefPlanes);
+
+        var notVisibleFacesId = new List<RD.Id>();
+        var notVisibleFaces = new List<RD.PlanarFace>();
+        var notvisibleRefPlanes = new HashSet<RD.ReferencePlane>();
+
+         // not visible faces
+        foreach (var face in facesRevit.Values)
+        {
+            if (!visibleFaceId.Contains(face.Id))
+            {
+                notVisibleFacesId.Add(face.Id);
+                notvisibleRefPlanes.Add(referencePlanesRevit[face.ReferencePlaneId]);
+                notVisibleFaces.Add(face);
+            }
+        }
+
+        RD.PlanarFace.WriteCsv(Path.Combine(projectPath, Stations.NotVisibleFacesFileName), notVisibleFaces);
+        RD.ReferencePlane.WriteCsv(Path.Combine(projectPath, Stations.NotVisibleRefPlanesFileName), notvisibleRefPlanes);
+
+        #endregion visible and not visible faces
+
+        Log.Information("visible and not visible faces");
+
+        #region color not visible faces
+
+        ElementId[] matId;
+
+        // add materials and save the ElementIds in a DataStorage
+        try
+        {
+            matId = Helper.AddMaterials(document);
+        }
+        catch (Exception)
+        {
+            matId = Helper.ReadMaterialsDS(document);
+        }
+
+        //Helper.Paint.ColourFace(doc, notVisibleFacesId, matId[0]);
+        int pMin = 1;
+        // TODO Test with minimum number of points on face
+        //var pMin = set.StepsPerFullTurn * set.StepsPerFullTurn * set.Beta_Degree / 25000;
+        Log.Information(pMin.ToString() + " pMin");
+        // Test if a minimum number has been reached
+        var visibleWithPMin = new List<RD.Id>();
+        foreach (var pf in test)
+        {
+            if (pf.Value >= pMin)
+            {
+                visibleWithPMin.Add(pf.Key);
+                //Log.Information(pf.Key.ToString());
+                //Log.Information(pf.Value.ToString());
+            }
+            //else
+            //{
+            //    //Log.Information(pf.Value.ToString());
+            //}
+        }
+
+        Helper.Paint.ColourFace(document, visibleWithPMin, matId[5]);
+
+        #endregion color not visible faces
+
+        Log.Information("color not visible faces");
+        TaskDialog.Show("Message", "finish");
+        Log.Information("end Stations2NotVisibleFaces");
+        return Result.Succeeded;
     }
 }
